641724f7cea50075adb6003a75e22823ca549f40
pallavi526 <71433453+pallavi526@users.noreply.github.com>
2022-06-28 13:18:38 +0530

    Add files via upload

---
+++ screen.py
@@ -0,0 +1,22 @@
+from turtle import width
+from PIL import ImageGrab
+import numpy as np
+import cv2
+#from win32api import GetSystemMetrics
+
+#width = GetSystemMetrics(0)
+#height = GetSystemMetrics(1)
+
+#print(width, height)
+
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+capture_video = cv2.VideoWriter('output.mp4', fourcc, 10.0, (1350, 720))
+
+while True:
+    img = ImageGrab.grab(bbox=(0, 0, 1350, 720))
+    img_np = np.array(img)
+    img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Screen Recorder', img_final)
+    capture_video.write(img_final)
+    if cv2.waitKey(10) == ord('q'):
+        break
